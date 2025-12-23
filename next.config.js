fdaa6ca6a84b38d41d1f52ffbc2e5f40b94be8f7
qbakom <qbakom@gmail.com>
2025-12-24 00:49:51 +0100

    chore: configure github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoBase = '/RepoTyper';
+
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
+    basePath: repoBase,
+    assetPrefix: `${repoBase}/`,
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
